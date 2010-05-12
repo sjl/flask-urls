47e21119621f211b0cac47972f5ad7ca92ffd950
Steve Losh <steve@stevelosh.com>
2010-05-11 21:09:35 -0400

    Add the actual functionality, lol.

--- flaskext/urls.py
+++ flaskext/urls.py
@@ -8,3 +8,17 @@
     :copyright: (c) 2010 by Steve Losh.
     :license: MIT, see LICENSE for more details.
 """
+
+from flask import url_for
+from werkzeug.routing import BuildError
+
+def permalink(function):
+    def inner(*args, **kwargs):
+        endpoint, values = function(*args, **kwargs)
+        try:
+            return url_for(endpoint, **values)
+        except BuildError:
+            return
+    return inner
+
+
